1a38fa39b20d6e5ff24014fe2b075a1f8541bc2a
martyopants@gmail.com <Marty OConnor>
2022-01-12 16:49:14 -0500

    Refinements

--- src/utils/useKeyPress.js
+++ src/utils/useKeyPress.js
@@ -1,35 +1,5 @@
 import { useState, useEffect } from "react";
 
-function useKeyPress(targetKey) {
 
-	const [keyPressed, setKeyPressed] = useState(false);
-
-	const [keysThatHaveBeenPressed, setkeysThatHaveBeenPressed] = useState(['test']);
-	console.log(keysThatHaveBeenPressed);
-
-	function downHandler({ key }) {
-	  if (key === targetKey) {
-		setKeyPressed(true);
-	  }
-	}
-
-	const upHandler = ({ key }) => {
-	  if (key === targetKey) {
-		setKeyPressed(false);
-	  }
-	};
-
-	useEffect(() => {
-	  window.addEventListener("keydown", downHandler);
-	  window.addEventListener("keyup", upHandler);
-
-	  // Remove event listeners on cleanup
-	  return () => {
-		window.removeEventListener("keydown", downHandler);
-		window.removeEventListener("keyup", upHandler);
-	  };
-	}, []);
-	return keyPressed;
-}
 
 export default useKeyPress;
