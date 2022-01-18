881ab42032b35bbbe186342b440e551b0a1b65f9
martyopants@gmail.com <Marty OConnor>
2022-01-18 15:19:46 -0500

    WIP

--- src/App.js
+++ src/App.js
@@ -1,10 +1,45 @@
 import './App.css';
 import React from 'react';
-import { useState, useEffect } from "react";
+import { useState, useEffect, useRef } from "react";
 import { v4 as uuidv4 } from 'uuid';
 import { useGamepads } from 'react-gamepads';
+import debounce from 'lodash.debounce';
+import AwesomeDebouncePromise from 'awesome-debounce-promise'
+
+
+// const myGamepad = navigator.getGamepads();
+// console.log(myGamepad[0])
+
+// window.addEventListener("gamepadbuttondown", function(e){
+// 	// Button down
+// 	console.log(
+
+// 	   "Button down",
+// 	   e.button, // Index of button in buttons array
+// 	   e.gamepad
+
+// 	);
+//  });
+
+// setInterval(() => {
+//     const myGamepad = navigator.getGamepads()[0];
+//     console.log(`Left stick at (${myGamepad.axes[0]}, ${myGamepad.axes[1]})` );
+//     console.log(`Right stick at (${myGamepad.axes[2]}, ${myGamepad.axes[3]})` );
+// }, 100)
+
 
 function KeysPressed() {
+
+	const [gamepads, setGamepads] = useState([]);
+	const [gamepad, setGamepad] = useState(() => navigator.getGamepads()[0]);
+
+	//console.log('gamepad', gamepad)
+
+	// useEffect(() => {
+
+	// 	console.log(gamepad)
+	// }, [gamepad])
+
 
 
 	const [keysArr, setKeysArr] = useState(
@@ -13,19 +48,34 @@ function KeysPressed() {
 
 	const allowedKeys = ['x', 'y', 'a', 'b'];
 
+	let debouncedKey = useRef('test');
+
 	function updateKeysArr(uppercaseKey) {
 		setKeysArr(keysArr => [...keysArr, uppercaseKey])
 	}
+
+	useGamepads(gamepads => {
+		setGamepads(Object.values(gamepads))
+	})
 
 	useEffect(() => {
 		window.localStorage.setItem('buttons', JSON.stringify(keysArr))
 	}, [keysArr])
 
 	function testKeyInput({ key }) {
-
+		console.log(key)
 		if ( allowedKeys.includes(key.toLowerCase()) ) {
 			const uppercaseKey = key.toUpperCase();
 			updateKeysArr(uppercaseKey);
+		}
+	}
+
+	function testGamepadInput( button ) {
+		console.log(button)
+		if ( allowedKeys.includes(button.toLowerCase()) ) {
+			const uppercaseKey = button.toUpperCase();
+			updateKeysArr(uppercaseKey);
+
 		}
 	}
 
@@ -38,9 +88,34 @@ function KeysPressed() {
 		};
 	  }, []);
 
-	useEffect(() => {
+	// useEffect(() => {
+	// 	if(gamepads[0]) {
+	// 		console.log(gamepads[0].buttons[0])
+	// 		if(gamepads[0].buttons[0]) {
+	// 			//console.log(gamepads[0].buttons[0])
+	// 			// const test = testGamepadInput('a');
+	// 			// console.log(test)
+	// 			testGamepadInput('a')
+	// 			//return AwesomeDebouncePromise(() => testGamepadInput('a'), 500)
+	// 		}
+	// 		//console.log(gamepads[0].buttons[0].pressed)
+	// 	}
+	// }, [gamepads[0]])
 
-	})
+	useEffect(() => {
+		if(gamepad) {
+			// console.log(gamepad[0].buttons[0])
+			console.log(gamepad.buttons)
+			// if(gamepad[0].buttons[0]) {
+			// 	//console.log(gamepad[0].buttons[0])
+			// 	// const test = testGamepadInput('a');
+			// 	// console.log(test)
+			// 	//testGamepadInput('a')
+			// 	//return AwesomeDebouncePromise(() => testGamepadInput('a'), 500)
+			// }
+			//console.log(gamepad[0].buttons[0].pressed)
+		}
+	}, [gamepad])
 
 
 	const div = keysArr.map( (item) => {
@@ -57,15 +132,15 @@ function KeysPressed() {
 
 function App() {
 
-	const [gamepads, setGamepads] = useState([]);
+	// const [gamepads, setGamepads] = useState([]);
 
-	useGamepads(gamepads => {
-		setGamepads(Object.values(gamepads))
-	})
+	// useGamepads(gamepads => {
+	// 	setGamepads(Object.values(gamepads))
+	// })
 
-	if(gamepads && gamepads[0]) {
-		console.log(gamepads[0].buttons[0].pressed)
-	}
+	// if(gamepads && gamepads[0]) {
+	// 	console.log(gamepads[0].buttons[0].pressed)
+	// }
 	// useGamepads( gamepads => setGamepads(gamepads));
 	// console.log(gamepads[0].buttons)
 
@@ -73,7 +148,7 @@ function App() {
     <div className="App">
 		<div className="container">
 			<div className="info">Connect your controller.
-			{gamepads.length && gamepads.map(gp => {
+			{/* {gamepads.length && gamepads.map(gp => {
 				return (
 					<div>
 						<div><span>ID:</span>{gp.id}</div>
@@ -84,7 +159,7 @@ function App() {
 						})}
 					</div>
 				)
-			})}
+			})} */}
 			</div>
 			<div className="button-area">
 				{ KeysPressed() }
