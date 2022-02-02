fd6ee08c8ce0413ace75a00a205dbe6618fc33cd
martyopants@gmail.com <Marty OConnor>
2022-02-02 17:26:26 -0500

    Remove some unused code

--- src/components/ConnectYourController.js
+++ src/components/ConnectYourController.js
@@ -1,19 +1,4 @@
-import { useEffect } from "react";
-
-
-function ConnectYourController({ gamePadState, clickRef }) {
-	//console.log(gamePadState)
-	useEffect(() => {
-
-
-
-		//const gamePadArr = Object.values(gamePadState);
-
-		// if (gamePadArr[0] !== null) {
-		// 	clickRef.current = gamePadArr[0];
-		// }
-
-	}, [clickRef])
+function ConnectYourController({ clickRef }) {
 
 	return (
 		<div className="info">{clickRef.current.id ? clickRef.current.id : 'Connect your controller'}</div>
